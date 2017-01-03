54ffe5831748a1c2165fca20a2457e5761bd0127
Chris Cooper <cjcooper252@gmail.com>
2017-01-03 15:24:26 +0000

    list creates note

--- note-list-model.js
+++ note-list-model.js
@@ -4,13 +4,22 @@
     this.notes = [];
   }
 
-  List.prototype.getNotes = function(){
-    return this.notes;
+  List.prototype = {
+
+    getNotes: function(){
+      return this.notes;
+    },
+
+    addNote: function(note){
+      this.notes.push(note);
+    },
+
+    createNote: function(noteText){
+      var note = new Note(noteText);
+      this.addNote(note);
+    }
   }
 
-  List.prototype.addNote = function(note){
-    this.notes.push(note);
-  }
 
 
   exports.List = List;
--- note-model-tests.js
+++ note-model-tests.js
@@ -13,12 +13,22 @@ function testListHasArray() {
 }
 
 function testListStoredNotes() {
-  var note = new Note(noteText1);
+  var note1 = new Note(noteText1);
+  var note2 = new Note(noteText2);
   var list = new List();
-  list.addNote(note)
-  assert.isTrue( list.getNotes().includes(note) )
+  list.addNote(note1);
+  list.addNote(note2);
+  assert.isTrue( list.getNotes().includes(note1, note2) );
+}
+
+function testListCreatesAndStoresNote() {
+  var list = new List();
+  list.createNote(noteText1)
+  var note = list.notes[0]
+  assert.isTrue( note.getText() === noteText1 )
 }
 
 testNoteInstantiatedWithText();
 testListHasArray();
 testListStoredNotes();
+testListCreatesAndStoresNote()
