d52ce2b2feb8a44470c1e01155eb672e848c2cc3
Pea M. Tyczynska <CrystalPea@github.com>
2017-01-05 11:21:05 +0000

    Note has an id property that can be read from outside

--- spec/note-model-spec.js
+++ spec/note-model-spec.js
@@ -6,6 +6,16 @@ testNoteCreation();
 
 function testReturnNoteText() {
   var note = new Note("test");
-  assert.isTrue(note.returnNoteText() === note._text)
+  assert.isTrue(note.returnNoteText() === note._text);
 };
 testReturnNoteText();
+
+(function testCreationWithId() {
+  var note = new Note("test", 1);
+  assert.isTrue(note._id === 1);
+})();
+
+(function testReturnId() {
+  var note = new Note("test", 1);
+  assert.isTrue(note.returnId() === 1);
+})();
--- src/models/note-model.js
+++ src/models/note-model.js
@@ -1,11 +1,16 @@
 (function(exports) {
 
-	function Note(string){
+	function Note(string, n){
 	  this._text = string;
+		this._id = n
 	}
 
 	Note.prototype.returnNoteText = function() {
 		return this._text;
+	};
+
+	Note.prototype.returnId = function() {
+		return this._id;
 	};
 
 	exports.Note = Note;
