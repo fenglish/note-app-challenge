861c3b2f049f1a029183fc094cbc213d87e65b17
Asuka Ochi <asukaochi@gmail.com>
2017-01-06 09:01:10 +0000

    this commit for sort out merge confrict

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 var listView = new ListView();
 NoteController.createList();
-NoteController.list.add("Favourite drink: seltzer");
-NoteController.list.add("Favourite food: Banana");
 var listHtml = listView.changeToHtml(NoteController.list);
 NoteController.showList(listHtml);
 NoteController.makeUrlChangeIdForCurrentPage();
