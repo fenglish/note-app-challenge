a58e72a5fcc466d5e2605756276898cf05d24955
Asuka Ochi <asukaochi@gmail.com>
2017-01-06 14:52:20 +0000

    refactoring tests with mocks

--- spec/note-controller-spec.js
+++ spec/note-controller-spec.js
@@ -1,6 +1,23 @@
-noteList = new NoteList();
-noteList.createNote("Favourite drink: margarita");
+// doubles
+note = {};
+note.returnNoteText = function(){ return "Favourite drink: margarita";};
+note.returnId = function(){ return 0; };
 
+noteList = {};
+noteList.showNotes = function(){ return [note]; };
+noteList.createNote = function(){ return note; };
+noteList.findNote = function(){ return note; };
+
+note2 = {};
+note2.returnNoteText = function(){ return "Favourite food: banana";};
+note2.returnId = function(){ return 0; };
+
+noteList2 = {};
+noteList2.showNotes = function(){ return [note2]; };
+noteList2.createNote = function(){ return note2; };
+noteList2.findNote = function(){ return note2; };
+
+// tests
 (function testInstantiateNoteController() {
   var controller = new NoteController(noteList);
   assert.isTrue(typeof controller === "object", testInstantiateNoteController.name)
@@ -8,7 +25,6 @@ noteList.createNote("Favourite drink: margarita");
 
 (function testUpdateHTML() {
   var controller = new NoteController(noteList);
-
   controller.addNoteListView();
   controller.updateHTML("app");
   assert.isTrue(app.innerHTML === "<ul><li><a href=\"#0\">Favourite drink: ma</a></li></ul>", testUpdateHTML.name)
@@ -28,8 +44,8 @@ noteList.createNote("Favourite drink: margarita");
 })();
 
 (function testNoteCreation(){
-  var controller = new NoteController(noteList);
-  controller.addNoteListView(noteList);
+  var controller = new NoteController(noteList2);
+  controller.addNoteListView(noteList2);
   controller.updateHTML("app");
   var textarea = document.getElementById("textarea")
   textarea.value = "Favourite food: banana"
