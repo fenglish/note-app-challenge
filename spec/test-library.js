5c5d4536c1edf34d59fd78855396240c224bc5a2
Asuka Ochi <asukaochi@gmail.com>
2017-01-06 14:21:50 +0000

    refactoring tests to show function name when they failed

--- spec/test-library.js
+++ spec/test-library.js
@@ -1,7 +1,7 @@
 var assert = {
   isTrue: function(assertionToCheck, functionName) {
     if (!assertionToCheck) {
-      throw new Error("Assertion failed: " + assertionToCheck + " is not truthy (ಠ_ಠ)");
+      throw new Error(functionName+ " failed:" + assertionToCheck + " is not truthy (ಠ_ಠ)");
     } else {
       return console.log(functionName + " says (ﾉ◕ヮ◕)ﾉ")
     }
